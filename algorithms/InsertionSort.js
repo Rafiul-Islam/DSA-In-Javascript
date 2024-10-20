db005cc9b79deb86978fc7e1a0a3e9a0f634f51b
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-10-21 00:32:07 +0600

    Insertion Sort

---
+++ algorithms/InsertionSort.js
@@ -0,0 +1,15 @@
+function InsertionSort(list) {
+  for (let i = 1; i < list.length; i++) {
+    let current = list[i];
+    var j = i - 1;
+    while (j >= 0 && list[j] > current) {
+      list[j + 1] = list[j];
+      j--;
+    }
+    list[j + 1] = current;
+  }
+  return list;
+}
+
+const list = [8, 2, 4, 1, 3];
+console.log(InsertionSort(list));
